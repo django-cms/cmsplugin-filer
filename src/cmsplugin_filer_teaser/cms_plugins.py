7e120e964397cb0a4d25b1ab7af429a48b0b81cc
stefanfoulis <stefan.foulis@gmail.com>
2010-01-28 12:29:11 +0100

    fixed bug in teaser when image is missing

--- src/cmsplugin_filer_teaser/cms_plugins.py
+++ src/cmsplugin_filer_teaser/cms_plugins.py
@@ -10,29 +10,30 @@ class FilerTeaserPlugin(CMSPluginBase):
     render_template = "cmsplugin_filer_teaser/teaser.html"
     
     def render(self, context, instance, placeholder):
-        # TODO: this scaling code needs to be in a common place
-        placeholder_width = context.get('width', None)
-        if instance.use_autoscale and placeholder_width:
-            width = placeholder_width
-        else:
-            if instance.width:
-                width = instance.width
+        if instance.image:
+            # TODO: this scaling code needs to be in a common place
+            placeholder_width = context.get('width', None)
+            if instance.use_autoscale and placeholder_width:
+                width = placeholder_width
             else:
-                width = instance.image.width
-        if instance.height:
-            height = instance.height
-            if width == instance.image.width:
-                # width was not externally defined: use ratio to scale it by the height
-                width = int( float(height)*float(instance.image.width)/float(instance.image.height) )
-        else:
-            # height was not externally defined: use ratio to scale it by the width
-            height = int( float(width)*float(instance.image.height)/float(instance.image.width) )
-
+                if instance.width:
+                    width = instance.width
+                else:
+                    width = instance.image.width
+            if instance.height:
+                height = instance.height
+                if width == instance.image.width:
+                    # width was not externally defined: use ratio to scale it by the height
+                    width = int( float(height)*float(instance.image.width)/float(instance.image.height) )
+            else:
+                # height was not externally defined: use ratio to scale it by the width
+                height = int( float(width)*float(instance.image.height)/float(instance.image.width) )
+            context.update({'image_size': u'%sx%s' % (width, height),})
+            print width, height
         context.update({
             'object':instance, 
             'placeholder':placeholder,
             'link':instance.link,
-            'image_size': u'%sx%s' % (width, height),
         })
         return context
 plugin_pool.register_plugin(FilerTeaserPlugin)
