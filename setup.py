d49d455f2be956c39d994aa33b5dac62754105c1
stefanfoulis <stefan.foulis@gmail.com>
2010-05-18 01:37:33 +0200

    added forgotten migration, bump to 0.0.6a1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "cmsplugin-filer",
-    version = "0.0.5a9",
+    version = "0.0.6a1",
     url = 'http://github.com/stefanfoulis/cmsplugin-filer',
     license = 'BSD',
     description = "django-cms plugins for django-filer",
