bded67a03fd32a6e2b2ffb608316d8741f610acb
ojii <jonas.obrist@divio.ch>
2010-05-18 11:45:52 +0200

    added missing migration

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "cmsplugin-filer",
-    version = "0.0.6a2",
+    version = "0.0.6a3",
     url = 'http://github.com/stefanfoulis/django-filer-cmsplugins',
     license = 'BSD',
     description = "django-cms plugins for django-filer",
