8ac5a253b414a9770e1fb4f8cdc293dd6b0a0348
stefanfoulis <stefan.foulis@gmail.com>
2010-06-04 18:36:24 +0200

    version bump to 0.0.6a2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "cmsplugin-filer",
-    version = "0.0.6a1",
+    version = "0.0.6a2",
     url = 'http://github.com/stefanfoulis/cmsplugin-filer',
     license = 'BSD',
     description = "django-cms plugins for django-filer",
