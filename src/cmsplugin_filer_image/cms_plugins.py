4656246c82937417d754d7168435d06f04bdb591
stefanfoulis <stefan.foulis@gmail.com>
2010-06-04 18:35:03 +0200

    added support for optional external image url to image plugin, default teaser template

--- src/cmsplugin_filer_image/cms_plugins.py
+++ src/cmsplugin_filer_image/cms_plugins.py
@@ -1,8 +1,11 @@
+import os
 from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
 from django.utils.translation import ugettext_lazy as _
 import models
 from django.conf import settings
+
+from filer.settings import FILER_ADMIN_ICON_SIZES, FILER_PUBLICMEDIA_PREFIX, FILER_PRIVATEMEDIA_PREFIX, FILER_STATICMEDIA_PREFIX
 
 class FilerImagePlugin(CMSPluginBase):
     model = models.FilerImage
@@ -10,26 +13,30 @@ class FilerImagePlugin(CMSPluginBase):
     render_template = "cmsplugin_filer_image/image.html"
     text_enabled = True
     raw_id_fields = ('image',)
+    admin_preview = False
     
     def render(self, context, instance, placeholder):
         # TODO: this scaling code needs to be in a common place
         # use the placeholder width as a hint for sizing
         placeholder_width = context.get('width', None)
-        if instance.use_autoscale and placeholder_width:
-            width = placeholder_width
-        else:
-            if instance.width:
-                width = instance.width
+        if instance.image:
+            if instance.use_autoscale and placeholder_width:
+                width = placeholder_width
             else:
-                width = instance.image.width
-        if instance.height:
-            height = instance.height
-            if width == instance.image.width:
-                # width was not externally defined: use ratio to scale it by the height
-                width = int( float(height)*float(instance.image.width)/float(instance.image.height) )
+                if instance.width:
+                    width = instance.width
+                else:
+                    width = instance.image.width
+            if instance.height:
+                height = instance.height
+                if width == instance.image.width:
+                    # width was not externally defined: use ratio to scale it by the height
+                    width = int( float(height)*float(instance.image.width)/float(instance.image.height) )
+            else:
+                # height was not externally defined: use ratio to scale it by the width
+                height = int( float(width)*float(instance.image.height)/float(instance.image.width) )
         else:
-            # height was not externally defined: use ratio to scale it by the width
-            height = int( float(width)*float(instance.image.height)/float(instance.image.width) )
+            width, height = instance.width, instance.height
         
         context.update({
             'object':instance,
@@ -40,5 +47,8 @@ class FilerImagePlugin(CMSPluginBase):
         })
         return context
     def icon_src(self, instance):
-        return instance.image.thumbnails['admin_tiny_icon']
+        if instance.image:
+            return instance.image.thumbnails['admin_tiny_icon']
+        else:
+            return os.path.normpath(u"%s/icons/missingfile_%sx%s.png" % (FILER_STATICMEDIA_PREFIX, 32, 32,))
 plugin_pool.register_plugin(FilerImagePlugin)
--- src/cmsplugin_filer_image/models.py
+++ src/cmsplugin_filer_image/models.py
@@ -16,7 +16,8 @@ class FilerImage(CMSPlugin):
                      (RIGHT, _("right")),
                      )
     caption = models.CharField(null=True, blank=True, max_length=255)
-    image = FilerImageField()
+    image = FilerImageField(null=True, blank=True, default=None)
+    image_url = models.URLField(_("alternative image url"), verify_exists=False, null=True, blank=True, default=None)
     alt_text = models.CharField(null=True, blank=True, max_length=255)
     
     use_autoscale = models.BooleanField(_("use automatic scaling"), default=True, 
@@ -31,14 +32,13 @@ class FilerImage(CMSPlugin):
                                   help_text=_("if present image will be clickable"))
     description = models.TextField(_("description"), blank=True, null=True)
     
-    '''
-    @property
-    def scaled_image_url(self):
-        h = self.height or self.image.width
-        w = self.width or self.image.height
-        tn = unicode(DjangoThumbnail(self.image.file, (w,h), opts=['crop','upscale'] ))
-        return tn
-    '''
+    def clean(self):
+        from django.core.exceptions import ValidationError
+        # Make sure that either image or image_url is set
+        if (not self.image and not self.image_url) or (self.image and self.image_url):
+            raise ValidationError(_('Either an image or an image url must be selected.'))
+
+    
     def __unicode__(self):
         if self.image:
             return self.image.label
