09f47ef4990b1a3158aa9c5d99cfa7f3b27b2a92
stefanfoulis <stefan.foulis@gmail.com>
2010-06-08 15:04:40 +0200

    fixed error in image template, version bump to 0.0.6a9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "cmsplugin-filer",
-    version = "0.0.6a8",
+    version = "0.0.6a9",
     url = 'http://github.com/stefanfoulis/cmsplugin-filer',
     license = 'BSD',
     description = "django-cms plugins for django-filer",
