1eb271d5abd14af090d52c0aa4fde37997443911
Yann Malet <yann.malet@gmail.com>
2010-09-04 14:22:59 +0200

    Start to bring the approriate information in the context of the folder.html

--- src/cmsplugin_filer_folder/cms_plugins.py
+++ src/cmsplugin_filer_folder/cms_plugins.py
@@ -10,9 +10,37 @@ class FilerFolderPlugin(CMSPluginBase):
     render_template = "cmsplugin_filer_folder/folder.html"
     text_enabled = True
     
-    def render(self, context, instance, placeholder):  
+    def get_folder_files(self, folder, user):
+        qs_files = folder.files.filter(image__isnull=True)
+        if user.is_staff:
+            return qs_files
+        else:
+            return qs_files.filter(is_public=True)
+    
+    def get_folder_images(self, folder, user):
+        qs_files = folder.files.filter(image__isnull=False)
+        if user.is_staff:
+            return qs_files
+        else:
+            return qs_files.filter(is_public=True)
+    
+    def get_children(self, folder):
+        return folder.get_children()
+    
+    def render(self, context, instance, placeholder):
+        
+        #import ipdb; ipdb.set_trace()
+        folder_files = self.get_folder_files(instance.folder,
+                                             context['request'].user)
+        folder_images = self.get_folder_images(instance.folder,
+                                               context['request'].user)
+        folder_folders = self.get_children(instance.folder)
+        
         context.update({
-            'object':instance, 
+            'object':instance,
+            'folder_files': folder_files,
+            'folder_images': folder_images,
+            'folder_folders': folder_folders,
             'placeholder':placeholder
         })    
         return context
