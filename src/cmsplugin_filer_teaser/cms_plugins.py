ff1ade137046dcc2ab6fbd082bb3a377e6c03753
stefanfoulis <stefan.foulis@gmail.com>
2010-10-10 21:16:50 +0200

    cmsplugin_filer_teaser fixes

--- src/cmsplugin_filer_teaser/cms_plugins.py
+++ src/cmsplugin_filer_teaser/cms_plugins.py
@@ -6,36 +6,58 @@ from django.conf import settings
 
 
 class FilerTeaserPlugin(CMSPluginBase):
+    """
+    TODO: this plugin is becoming very similar to the image plugin... code 
+          should be re-used somehow.
+    """
     model = models.FilerTeaser
     name = _("Teaser (Filer)")
     render_template = "cmsplugin_filer_teaser/teaser.html"
     
-    def render(self, context, instance, placeholder):
+    def _get_thumbnail_options(self, context, instance):
+        """
+        Return the size and options of the thumbnail that should be inserted
+        """
+        width, height = None, None
+        subject_location = False
+        placeholder_width = context.get('width', None)
+        if instance.use_autoscale and placeholder_width:
+            # use the placeholder width as a hint for sizing
+            width = int(placeholder_width)
+        elif instance.width:
+            width = instance.width
+        if instance.height:
+            height = instance.height
         if instance.image:
-            # TODO: this scaling code needs to be in a common place
-            placeholder_width = context.get('width', None)
-            if instance.use_autoscale and placeholder_width:
-                width = placeholder_width
-            else:
-                if instance.width:
-                    width = instance.width
-                else:
-                    width = instance.image.width
-            if instance.height:
-                height = instance.height
-                if width == instance.image.width:
-                    # width was not externally defined: use ratio to scale it by the height
-                    width = int( float(height)*float(instance.image.width)/float(instance.image.height) )
-            else:
+            if instance.image.subject_location:
+                subject_location = instance.image.subject_location
+            if not height and width:
                 # height was not externally defined: use ratio to scale it by the width
                 height = int( float(width)*float(instance.image.height)/float(instance.image.width) )
-            context.update({'image_size': u'%sx%s' % (width, height),
-                            'image_width': width,
-                            'image_height': height})
+            if not width and height:
+                # width was not externally defined: use ratio to scale it by the height
+                width = int( float(height)*float(instance.image.width)/float(instance.image.height) )
+            if not width:
+                # width is still not defined. fallback the actual image width
+                width = instance.image.width
+            if not height:
+                # height is still not defined. fallback the actual image height
+                height = instance.image.height
+        return {'size': (width, height),
+                'subject_location': subject_location}
+       
+    def get_thumbnail(self, context, instance):
+        if instance.image:
+            return instance.image.image.file.get_thumbnail(self._get_thumbnail_options(context, instance))
+    
+    def render(self, context, instance, placeholder):
+        options = self._get_thumbnail_options(context, instance)
         context.update({
-            'instance':instance, 
-            'placeholder':placeholder,
+            'instance': instance,
             'link': instance.link,
+            'opts': options,
+            'size': options.get('size',None),
+            'placeholder': placeholder
         })
         return context
 plugin_pool.register_plugin(FilerTeaserPlugin)
