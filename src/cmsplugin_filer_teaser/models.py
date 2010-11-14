e7e600c6ca7dc2f1afbafc982619f20527902177
Simon Hedberg <simon@in-formation.se>
2010-11-14 23:03:09 +0100

    Revert "added missing field names translation strings"
    This reverts commit 2e3bd0f874b850f61da080bb03ab6949aeebe2a6.

--- src/cmsplugin_filer_teaser/models.py
+++ src/cmsplugin_filer_teaser/models.py
@@ -12,15 +12,15 @@ class FilerTeaser(CMSPlugin):
     A Teaser
     """
     title = models.CharField(_("title"), max_length=255, blank=True)
-    image = FilerImageField(_("image"), blank=True, null=True)
+    image = FilerImageField(blank=True, null=True)
     image_url = models.URLField(_("alternative image url"), verify_exists=False, null=True, blank=True, default=None)
     
     style = models.CharField(_("teaser style"), max_length=255, null=True, blank=True, choices=CMSPLUGIN_FILER_TEASER_STYLE_CHOICES)
     
     use_autoscale = models.BooleanField(_("use automatic scaling"), default=True, 
                                         help_text=_('tries to auto scale the image based on the placeholder context'))
-    width = models.PositiveIntegerField(_("width"), null=True, blank=True)
-    height = models.PositiveIntegerField(_("height"), null=True, blank=True)
+    width = models.PositiveIntegerField(null=True, blank=True)
+    height = models.PositiveIntegerField(null=True, blank=True)
     
     free_link = models.CharField(_("link"), max_length=255, blank=True, null=True, help_text=_("if present image will be clickable"))
     page_link = PageField(verbose_name=_("page"), null=True, blank=True, help_text=_("if present image will be clickable"))
