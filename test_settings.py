fe3487a2efde396fee03d68ad54b450deac6ee08
stefanfoulis <stefan@foulis.ch>
2012-05-17 20:42:35 +0200

    fix CMS_TEMPLATES test settings for old cms versions

--- test_settings.py
+++ test_settings.py
@@ -37,6 +37,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     "django.core.context_processors.media",
     "django.contrib.messages.context_processors.messages"
 ]
+CMS_TEMPLATES = []
 
 ROOT_URLCONF = "test_urls"
 
