23f2be0d47c5bf8de48e614be6927dcbb5df06fc
Pavel Puchkin <neoascetic@gmail.com>
2012-08-23 20:44:00 +1100

    Revert "FilerFilePlugin.icon_src is also obsolete"
    This reverts commit 6f9609a350fcac101eb28c08f8499f11d753f292.

--- cmsplugin_filer_file/cms_plugins.py
+++ cmsplugin_filer_file/cms_plugins.py
@@ -1,9 +1,8 @@
-from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from cms.plugin_base import CMSPluginBase
 from django.utils.translation import ugettext_lazy as _
-
 import models
-
+from django.conf import settings
 
 class FilerFilePlugin(CMSPluginBase):
     module = 'Filer'
@@ -14,9 +13,14 @@ class FilerFilePlugin(CMSPluginBase):
 
     def render(self, context, instance, placeholder):
         context.update({
-            'object': instance,
-            'placeholder': placeholder
+            'object':instance,
+            'placeholder':placeholder
         })
         return context
+
+    def icon_src(self, instance):
+        file_icon = instance.get_icon_url()
+        if file_icon: return file_icon
+        return settings.CMS_MEDIA_URL + u"images/plugins/file.png"
 
 plugin_pool.register_plugin(FilerFilePlugin)
