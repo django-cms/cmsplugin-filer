5a6af039bbcaf289407de5ca034a85e919a27d8c
Alex Cucu <alexandruioan.cucu@gmail.com>
2013-01-14 18:47:59 +0200

    Reverted change to get_file_name; that's already covered by a different changeset

--- cmsplugin_filer_file/models.py
+++ cmsplugin_filer_file/models.py
@@ -30,11 +30,7 @@ class FilerFile(CMSPlugin):
         return self.file.file.storage.exists(self.file.path)
 
     def get_file_name(self):
-        if self.file.name in ('', None):
-            name = u"%s" % (self.file.original_filename,)
-        else:
-            name = u"%s" % (self.file.name,)
-        return name
+        return self.file.name
 
     def get_ext(self):
         return self.file.extension
