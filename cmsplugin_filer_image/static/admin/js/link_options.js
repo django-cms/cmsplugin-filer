c82cc25ea6229439a5d55c0def5d2d8cec9f0295
Marcel Cojocaru <marcel.cojocaru@threepillarglobal.com>
2013-10-01 11:17:36 +0300

    LUN-1070 -- added comments

--- cmsplugin_filer_image/static/admin/js/link_options.js
+++ cmsplugin_filer_image/static/admin/js/link_options.js
@@ -8,11 +8,16 @@
         for (item in formset_divs_cls) {
             $(formset_divs_cls[item]).hide();
         }
-        var option = value ? value : $('#id_link_options option[selected="selected"]').val();
-        $(formset_divs_cls[option]).show();
+        var link_option = value ? value : $('#id_link_options option[selected="selected"]').val();
+        $(formset_divs_cls[link_option]).show();
     };
 
     $(document).ready(function () {
+        // The "data" attribute (which value is dictionary) is set in cmsplugins.py,
+        // FilerImagePluginForm.__init__ method. The keys represents link_options
+        // values, and the values are css classes coresponding to the formsets that are
+        // shown/hidden. I've choosen this solution in order to avoid hard-coding
+        // link-options values.
         formset_divs_cls = JSON.parse($('#id_link_options').attr('data'));
         $('#id_link_options').change(function(){
             show_selected_link_option(this.value);
