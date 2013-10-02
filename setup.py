6e1d9bcfaf74b7d41b22147b9ba5e861543f6c90
Zoltán Szeredi <zoli.szeredi@gmail.com>
2013-10-02 17:10:15 +0300

    Bump version since there are commits since last tag.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "cmsplugin-filer",
-    version = "0.9.4pbs3",
+    version = "0.9.4pbs4",
     url = 'http://github.com/stefanfoulis/cmsplugin-filer',
     license = 'BSD',
     description = "django-cms plugins for django-filer",
