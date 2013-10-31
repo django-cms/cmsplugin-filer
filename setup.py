2c38572757bfda2508d370dd0c61da3d26e597b8
Marcel Cojocaru <marcel.cojocaru@threepillarglobal.com>
2013-10-31 10:55:35 +0200

    Bumped version as instructed by bamboo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "cmsplugin-filer",
-    version = "0.9.4pbs4",
+    version = "0.9.4pbs5",
     url = 'http://github.com/stefanfoulis/cmsplugin-filer',
     license = 'BSD',
     description = "django-cms plugins for django-filer",
