ef9283397a06554daaa9e5dcd413599cd156dee5
Tim Graham <timograham@gmail.com>
2014-01-25 14:05:00 -0500

    Removed useless version check.
    No longer serves a purpose after 48f8e0a9bc3d4ec3ff9ceaa7366641d9d036f15.

--- cmsplugin_filer_teaser/models.py
+++ cmsplugin_filer_teaser/models.py
@@ -5,7 +5,6 @@ from cms.models import CMSPlugin
 from cms.models.fields import PageField
 from filer.fields.image import FilerImageField
 from django.conf import settings
-from distutils.version import LooseVersion
 
 from cmsplugin_filer_utils import FilerPluginManager
 
@@ -17,32 +16,29 @@ class FilerTeaser(CMSPlugin):
     """
     title = models.CharField(_("title"), max_length=255, blank=True)
     image = FilerImageField(blank=True, null=True, verbose_name=_("image"))
-    if LooseVersion(django.get_version()) < LooseVersion('1.5'):
-        image_url = models.URLField(_("alternative image url"), null=True, blank=True, default=None)
-    else:
-        image_url = models.URLField(_("alternative image url"), null=True, blank=True, default=None)
-    
+    image_url = models.URLField(_("alternative image url"), null=True, blank=True, default=None)
+
     style = models.CharField(_("teaser style"), max_length=255, null=True, blank=True, choices=CMSPLUGIN_FILER_TEASER_STYLE_CHOICES)
-    
-    use_autoscale = models.BooleanField(_("use automatic scaling"), default=True, 
+
+    use_autoscale = models.BooleanField(_("use automatic scaling"), default=True,
                                         help_text=_('tries to auto scale the image based on the placeholder context'))
     width = models.PositiveIntegerField(_("width"), null=True, blank=True)
     height = models.PositiveIntegerField(_("height"), null=True, blank=True)
-    
+
     free_link = models.CharField(_("link"), max_length=255, blank=True, null=True, help_text=_("if present image will be clickable"))
     page_link = PageField(null=True, blank=True, help_text=_("if present image will be clickable"), verbose_name=_("page link"))
     description = models.TextField(_("description"), blank=True, null=True)
-    
+
     target_blank = models.BooleanField(_("open link in new window"), default=False)
 
     objects = FilerPluginManager(select_related=('image', 'page_link'))
-    
+
     def clean(self):
         from django.core.exceptions import ValidationError
         # Make sure that either image or image_url is set
         if self.image and self.image_url:
             raise ValidationError(_('Either an image or an image url must be selected.'))
-    
+
     def __unicode__(self):
         return self.title
 
