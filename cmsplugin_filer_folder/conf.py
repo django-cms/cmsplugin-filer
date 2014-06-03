d1258415a0bc3a8596c060f51b960dfb395af9f2
Stefan Foulis <stefan@foulis.ch>
2014-06-03 17:15:04 +0200

    adds selectable style for folder plugin

---
+++ cmsplugin_filer_folder/conf.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from appconf import AppConf
+
+
+class CmspluginFilerFolderAppConf(AppConf):
+    STYLE_CHOICES = (
+        ("list", _("List")),
+        ("slideshow", _("Slideshow"))
+    )
+    DEFAULT_STYLE = 'list'
+
+    def configure(self):
+        # set DEFAULT_STYLE to '' if it is not in STYLE_CHOICES
+        if not self.configured_data['DEFAULT_STYLE'] in [s for s, l in self.configured_data['STYLE_CHOICES']]:
+            self.configured_data['DEFAULT_STYLE'] = ''
+        return self.configured_data
