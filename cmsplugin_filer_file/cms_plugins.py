280de79d51f8b397bbf9c6b0c6233596a002dbeb
Stefan Foulis <stefan@foulis.ch>
2014-06-03 17:15:48 +0200

    adds selectable style for file plugin

--- cmsplugin_filer_file/cms_plugins.py
+++ cmsplugin_filer_file/cms_plugins.py
@@ -1,17 +1,34 @@
 from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
+from django.template.loader import select_template
 from django.utils.translation import ugettext_lazy as _
 import models
-from django.conf import settings
+from .conf import settings
 
 class FilerFilePlugin(CMSPluginBase):
     module = 'Filer'
     model = models.FilerFile
     name = _("File")
-    render_template = "cmsplugin_filer_file/file.html"
+    TEMPLATE_NAME = 'cmsplugin_filer_file/plugins/file/%s.html'
+    render_template = TEMPLATE_NAME % 'default'
     text_enabled = True
 
+    fieldsets = (
+        (None, {'fields': [
+            'title',
+            'file',
+            'target_blank'
+        ]}),
+    )
+    if settings.CMSPLUGIN_FILER_FILE_STYLE_CHOICES:
+        fieldsets[0][1]['fields'].append('style')
+
     def render(self, context, instance, placeholder):
+        self.render_template = select_template((
+            'cmsplugin_filer_file/plugins/file.html',  # backwards compatibility. deprecated!
+            self.TEMPLATE_NAME % instance.style,
+            self.TEMPLATE_NAME % 'default')
+        )
         context.update({
             'object':instance,
             'placeholder':placeholder
