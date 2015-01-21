45f62bfd5b30d0420f48a3f6aebcd80f7ddfe0e7
Peter J. Farrell <pfarrell@greatbiztools.com>
2015-01-21 03:40:00 -0600

    Update 0005_fix_table_names.py

--- cmsplugin_filer_folder/migrations/0005_fix_table_names.py
+++ cmsplugin_filer_folder/migrations/0005_fix_table_names.py
@@ -11,6 +11,10 @@ class Migration(SchemaMigration):
         # (old_name, new_name),
         ('cmsplugin_filerfolder', 'cmsplugin_filer_folder_filerfolder'),
     )
+    
+    needed_by = (
+        ("cms", "0069_static_placeholder_permissions"),
+    )
 
     def forwards(self, orm):
         rename_tables_old_to_new(db, self.cms_plugin_table_mapping)
