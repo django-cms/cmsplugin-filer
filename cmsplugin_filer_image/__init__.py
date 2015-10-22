5c3d1ec33f98a5fd5230f8a714214a7276f951d0
Venelin Stoykov <venelin@magicsolutions.bg>
2015-10-22 11:54:47 +0300

    Put django's AppConfig for nice name in django admin (Django 1.7+)

--- cmsplugin_filer_image/__init__.py
+++ cmsplugin_filer_image/__init__.py
@@ -1,2 +1,4 @@
 from cmsplugin_filer_file import __version__  # NOQA
 __authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
+
+default_app_config = 'cmsplugin_filer_image.apps.FilerImageConfig'
