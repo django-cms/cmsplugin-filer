d4e583fedbe34f89d0202f93d3ebf00d257876d5
Stefan Foulis <stefan@foulis.ch>
2016-03-07 13:37:58 +0100

    fix south migrations to work with newer django-cms
    newer versions of django-cms have switched from mptt to treebeard.
    Removes the anyway irrelevant fields related to 
    mptt from the frozen migration, so they don't cause problems.

--- cmsplugin_filer_folder/south_migrations/0003_move_view_option_to_style.py
+++ cmsplugin_filer_folder/south_migrations/0003_move_view_option_to_style.py
@@ -65,14 +65,14 @@ class Migration(DataMigration):
             'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
-            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            # 'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            # 'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
             'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
             'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
             'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+            # 'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            # 'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
         },
         'cms.placeholder': {
             'Meta': {'object_name': 'Placeholder'},
