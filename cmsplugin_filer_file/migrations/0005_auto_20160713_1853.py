b7d0c7f25e12868388e37de9eec951c372382a47
Jeffrey Goettsch <jgoettsch@gmail.com>
2016-08-21 08:44:55 -0700

    Add missing migrations for djangoCMS 3.3.1 (#255)

---
+++ cmsplugin_filer_file/migrations/0005_auto_20160713_1853.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cmsplugin_filer_file', '0004_auto_20160705_1334'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='filerfile',
+            name='cmsplugin_ptr',
+            field=models.OneToOneField(parent_link=True, related_name='cmsplugin_filer_file_filerfile', auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin'),
+        ),
+    ]
